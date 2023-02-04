a79027a8874bdda343bb2feddd6e0ede2a67b076
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2023-02-04 21:48:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -24,10 +24,37 @@ youtube = build("youtube", "v3", credentials=creds)
 video_links = ["https://youtu.be/42v9nCf5gzM", "https://www.youtube.com/watch?v=efgh5678"]
 
 for video_link in video_links:
-    # Download the video from YouTube
-    # ...
-
+    # Get the video ID from the link
+    video_id = video_link.split('v=')[1]
+    
+    # Get the video details from the YouTube Data API
+    request = youtube.videos().list(
+        part='snippet,status,statistics,contentDetails',
+        id=video_id
+    )
+    response = request.execute()
+    
+    # Get the video title, description, tags, and category ID
+    video_title = response['items'][0]['snippet']['title']
+    video_description = response['items'][0]['snippet']['description']
+    video_tags = response['items'][0]['snippet']['tags']
+    video_category_id = response['items'][0]['snippet']['categoryId']
+    
     # Upload the video to YouTube
-    # ...
-
-
+    request = youtube.videos().insert(
+        part='snippet,status,statistics,contentDetails',
+        body={
+            'snippet': {
+                'title': video_title,
+                'description': video_description,
+                'tags': video_tags,
+                'categoryId': video_category_id
+            },
+            'status': {
+                'privacyStatus': 'public'
+            }
+        },
+        media_body=f'/path/to/video/{video_id}.mp4'
+    )
+    response = request.execute()
+    print(f'Uploaded video with ID: {response["id"]}')
